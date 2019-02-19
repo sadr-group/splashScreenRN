d4ee5928d53e67acce62af4e14db90afd6a1b6a3
hjn <hjn_14@yahoo.com>
2019-02-19 17:02:32 +0330

    fristcommit

---
+++ android/app/src/main/java/com/splashexample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.splashexample;
+
+import android.os.Bundle;
+
+import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+
+public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    protected String getMainComponentName() {
+        return "SplashExample";
+    }
+}
